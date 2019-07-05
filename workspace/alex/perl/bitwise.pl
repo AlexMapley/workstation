737f364054814a1c7e784c1c1ada3ff8f1372970
AlexMapley <alexmapley@gmail.com>
2019-07-05 11:12:23 -0400

    more perl

--- workspace/alex/perl/bitwise.pl
+++ workspace/alex/perl/bitwise.pl
@@ -11,9 +11,11 @@ print $c, "\n";
 $c = $a | $b; # 0111 or 7
 print $c, "\n";
  
+# exclusive or
 $c = $a ^ $b; # 0110 or 6
 print $c, "\n";
  
+# not
 $c = ~$a; # 11111111111111111111111111111010 (64bits computer) or 4294967290
 print $c, "\n";
  
@@ -22,3 +24,18 @@ print $c, "\n";
  
 $c = $a << 1; # 0101 shift left 1 bit, 1010 or 10
 print $c, "\n";
+
+print $a <=> $b, "\n";
+
+
+$a = 10;
+$b = 20;
+ 
+print $a <=> $b, "\n";
+ 
+$b = 10;
+print $a <=> $b, "\n";
+ 
+$b = 5;
+print $a <=> $b, "\n";
+
---
+++ workspace/alex/perl/strings.pl
@@ -0,0 +1,15 @@
+
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+print "Learning the comparator" . " and concatenating" . "\n";
+
+print "a message " x 4, "\n";
+
+use warnings;
+use strict;
+ 
+my $s;
+chomp($s = <STDIN>);
+print $s, "\n";
