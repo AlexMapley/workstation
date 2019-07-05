4156df30a33f051cfeb0e1f90224fb65e1cd9e8a
AlexMapley <alexmapley@gmail.com>
2019-07-05 10:47:52 -0400

    numerics

---
+++ workspace/alex/perl/numerics.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+ 
+print 10 + 20, "\n"; # 20
+
+print 20 - 10, "\n"; # 10
+
+print 10 * 20, "\n"; # 200
+ 
+print 20 / 10, "\n"; # 2
+
+print 10 + 20/2 - 5 * 2 , "\n"; # 10
+
+print (((10 + 20)/2 - 5) * 2, "\n"); # 20;
