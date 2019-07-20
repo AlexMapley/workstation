159d2f265b72a7fb53f4216fbcfcaf97d0c8ddf9
AlexMapley <alexmapley@gmail.com>
2019-07-20 11:58:54 -0400

    python sys examples

--- workspace/alex/python/python-courses/sys_module.py
+++ workspace/alex/python/python-courses/sys_module.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python
 import sys
+
+print("\n", sys.version)
+
+## modify print to stdout to add a custom prepend
+def my_display(x):
+    print ("#>#> ")
+    print(x)
+sys.displayhook = my_display
+
+for i in range(len(sys.argv)):
+    if i == 0:
+        print("Function name: %s" % sys.argv[0])
+    else:
+        print("%d. argument: %s" % (i,sys.argv[i]))
+
+
+## Enumerta data streams
+for i in (sys.stdin, sys.stdout, sys.stderr):
+    print(i)
