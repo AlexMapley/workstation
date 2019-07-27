4b12720a6b87a1e5da014d99f821323467013f50
AlexMapley <alexmapley@gmail.com>
2019-07-27 16:29:29 -0400

    update run.sh documentation

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+# run.sh will compose a new workstation instance
+# with docker if it did not already exist.
+# Once done, or if already done previously,
+# it will begin a new bash user session via 
+# the user_daemon container.
 docker-compose up -d
 
 containerID=$(docker ps | grep ubuntu | awk '{print $1}')
