9b2e65cab312ed6b099f4c8b89113192ce0c812e
AlexMapley <alexmapley@gmail.com>
2019-08-20 15:48:57 -0400

    add reset bash script

---
+++ reset.sh
@@ -0,0 +1,13 @@
+## Checkout master, reset all commits, and delete all other branches
+git stash
+git stash --drop
+git reset --hard
+git checkout master
+git pull origin master
+git branch | grep -v master | xargs -L 1 git branch -D
+
+## Kill all containers
+docker ps | awk '{print $1}' | xargs -L -1 docker rm -f
+
+## Prune images and reset OS specs
+docker system prune -af
