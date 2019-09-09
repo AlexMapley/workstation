665629f370415f2c5918d8d70d58f678326f091f
AlexMapley <alexmapley@gmail.com>
2019-09-09 12:50:46 -0400

    comment out python and ocaml builds

--- setup/installs.sh
+++ setup/installs.sh
@@ -11,15 +11,15 @@ apt-get install install mysql-server -qq
 # Perl
 apt-get install rlwrap -qq
 
-# OCaml
-apt install ocaml -qq
+# # OCaml
+# apt install ocaml -qq
 
-# Python 3.5.6
-apt-get install build-essential checkinstall -qq
-## Will require timezone entry, 2-105 for America-NewYork
-apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -qq
-cd /usr/src
-wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
-tar xzf Python-3.5.6.tgz
-cd Python-3.5.6
-./configure --enable-optimizations
+# # Python 3.5.6
+# apt-get install build-essential checkinstall -qq
+# ## Will require timezone entry, 2-105 for America-NewYork
+# apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -qq
+# cd /usr/src
+# wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
+# tar xzf Python-3.5.6.tgz
+# cd Python-3.5.6
+# ./configure --enable-optimizations
