5d284a21d4f59442ef4caf41eef45ef0649e1432
AlexMapley <alexmapley@gmail.com>
2019-09-09 13:25:01 -0400

    more c tutorials

---
+++ workspace/alex/c/secure_intro/controls.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void) {
+	int powerLevel = 9000;
+
+	if (powerLevel > 9000) {
+		printf("It's over 9000!!!\n");
+	} else {
+		printf("guess not...\n");
+	}
+
+	return 0;
+}
