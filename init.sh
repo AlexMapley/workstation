03523fd76a01f97ced6e10468136d9f5ff274816
AlexMapley <alexmapley@gmail.com>
2019-09-13 15:26:03 -0400

    add ascii_art.pl to init and improve ascii art

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-perl setup/write_bashrc.pl && bash setup/installs.sh; source /root/.bashrc
+perl setup/write_bashrc.pl && bash setup/installs.sh; source /root/.bashrc; perl setup/ascii_art.pl 
--- workspace/alex/perl/misc/word_art.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-use warnings;
-
-print ("############################\n");
-print ("Welcome\n");
-print ("############################\n");
