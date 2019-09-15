d7318a93f300b2f9c73d3ff41eb057854356150b
AlexMapley <alexmapley@gmail.com>
2019-09-15 13:48:10 -0400

    c pointer receiver work

--- workspace/alex/c/secure_intro/swap.c
+++ workspace/alex/c/secure_intro/swap.c
@@ -5,13 +5,20 @@ void swap (int a, int b) {
 	b = 34;
 }
 
+void swap2 (int* a, int* b) {
+        *a = 35;
+        *b = 34;
+}
+
 int main (void) {
 	int x = 1;
 	int y = 2;
 
 	swap(x,y);
-
 	printf("x: %d\ny: %d\n", x, y);
+	
+	swap2(&x,&y);
+        printf("x: %d\ny: %d\n", x, y);
 
 	return 0;
 }
