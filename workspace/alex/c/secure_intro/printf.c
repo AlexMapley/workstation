19ddad730fa9b2dbe1467baaec7583cc4824dd36
AlexMapley <alexmapley@gmail.com>
2019-09-15 15:42:33 -0400

    more c printf vulnerability work

--- workspace/alex/c/secure_intro/printf.c
+++ workspace/alex/c/secure_intro/printf.c
@@ -8,6 +8,12 @@ int main (int agrc, char *argv[]) {
 		"stored in n\n", &n);
 	printf ("n is: %d\n", n);
 
+	printf ("Without width modifying: %c\n", 'A');
+	printf("Modifying with 10 minimum bytes: %10c\n", 'A');
+
+	printf ("4th parameter is '%4$c'\n",
+		'A', 0x42, 70, 0x23, 'a', 80);
+
 	return 0;
 
 }
