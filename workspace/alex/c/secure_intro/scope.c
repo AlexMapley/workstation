a5d52040812143f0cd6eebe6c7c08717555f3a69
AlexMapley <alexmapley@gmail.com>
2019-09-15 15:30:56 -0400

    more c tutorials

--- workspace/alex/c/secure_intro/scope.c
+++ workspace/alex/c/secure_intro/scope.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+int global = 68;
+
+void someFunction (void) {
+	global++;
+	puts ("Inside someFunction");
+	printf ("global is: %d\n", global);
+}
+
 int main (int argc, char *argv[]) {
 
 	int i = 42;
 	int j = 1337;
+
+	puts ("In main");
+	printf("gloabl is %d\n", global);
 
 	if (i == 42 && j == 1337) {
 		int i = 12;
@@ -14,6 +25,12 @@ int main (int argc, char *argv[]) {
 
 	puts ("Outside if statement");
 	printf ("i is : %d \nj is: %d\n",i,j);
+
+	someFunction();
+
+	puts ("In main");
+	printf("global is: %d\n", global);
+
 
 	return 0;
 
