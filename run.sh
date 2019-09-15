7ebf5fa7956d6df9fe9aa6aa9572d8bb7f3457b2
AlexMapley <alexmapley@gmail.com>
2019-09-15 14:29:31 -0400

    reformat ascii art files

--- run.sh
+++ run.sh
@@ -6,4 +6,7 @@
 docker-compose up -d
 
 containerID=$(docker ps | grep ubuntu | awk '{print $1}')
+
 docker exec -it $containerID /bin/bash
+docker exec -it $containerID bash "cd /src/ascii_art/scripts/ && bash display_random.sh"
+
