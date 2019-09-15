3b83903414cdec4b4ef959b86a10498c072094ed
AlexMapley <alexmapley@gmail.com>
2019-09-15 16:04:56 -0400

    finish vuln_print tutorial

--- workspace/alex/c/secure_intro/vuln_print.c
+++ workspace/alex/c/secure_intro/vuln_print.c
@@ -3,11 +3,15 @@
 
 int main (int argc, char *argv[]) {
 	char buf[21];
+	static int var = 42;
+
+	printf ("var is at [0x%08x]\n", &var);
 
 	strcpy (buf, argv[1]);
 	
 	printf (buf);
-	printf ("\n%s\n", buf);
+	
+	printf("\nvar is %d [0x%08x]\n", var, var);
 
 	return 0;
 }
