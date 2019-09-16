d82fd820461e7040af81e450c255ae3eaf0338c0
AlexMapley <alexmapley@gmail.com>
2019-09-16 14:30:05 -0400

    c tutorials: file writing and appending

--- workspace/alex/c/secure_intro/file_io.c
+++ workspace/alex/c/secure_intro/file_io.c
@@ -3,12 +3,16 @@
 int main (int agrc, char *argv[]) {
 	FILE *fp;
 	char buf [1024];
+	char buf2[] = "The foobar is the most foo of all the bars";
 
-	fp = fopen ("myfile", "r");
-
+	fp = fopen ("myfile", "a+");
 	fgets (buf, sizeof (buf), fp);
-
 	printf( "File contents: %s\n", buf);
+
+	fprintf (fp, "%s", buf2);
+	fgets(buf, sizeof(buf),fp);
+	printf("File contents: %s\n", buf);
+		
 
 	fclose (fp);
 
