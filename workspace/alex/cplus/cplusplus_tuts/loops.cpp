670a1db31ae79c51632d10ee0efdbe717403f39e
AlexMapley <alexmapley@gmail.com>
2019-09-17 16:27:14 -0400

    c++: more loop types

--- workspace/alex/cplus/cplusplus_tuts/loops.cpp
+++ workspace/alex/cplus/cplusplus_tuts/loops.cpp
@@ -3,11 +3,20 @@ using namespace std;
 
 int main () {
 	int n{10};
-
 	while (n > 0) {
 		cout << n << ", ";
 		--n;
 	}
-
 	cout << "liftoff!\n";
+
+	for (int k =10; k>0; k--) {
+		cout << k << ", ";
+	}
+
+	string str;
+	do {
+		cout << "Enter text: ";
+		getline (cin, str);
+		cout << "You entered: " << str << '\n';
+	} while (str != "goodbye");
 }
