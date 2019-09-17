d198f47d28a03b8c007c39e841d4b9a091064df0
AlexMapley <alexmapley@gmail.com>
2019-09-17 12:49:38 -0400

    c++: operators

---
+++ workspace/alex/cplus/cplusplus_tuts/operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int a, b;
+	a = 10;
+	b = 4;
+	a = b;
+	b = 7;
+
+	cout << "a: " << a;
+	cout << " b: " << b << "\n";
+
+	cout << (7==5 ? 4 : 3) << '\n';
+	cout << (7==7 ? 4 : 3) << '\n';
+
+
+}
