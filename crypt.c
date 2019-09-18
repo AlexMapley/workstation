6055848ecc1957b0d41ac671fb1393d4d6d872cc
AlexMapley <alexmapley@gmail.com>
2019-09-18 17:00:10 -0400

    start crypt.c

---
+++ crypt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define XOR_BYTE 0xA1
+
+void xorString (char *s) {
+	int i;
+	for (i = 0; s[i] != '\0'; i++) {
+		s[i] ^= XOR_BYTE;
+	}
+}
+
+// int main (int agrc, char *argv[]) {
+// 	char string[]= "Test string 123";
+
+// 	xorString (string);
+
+// 	printf ("Encrypted string: %s\n", string);
+
+// 	xorString (string);
+
+// 	printf("Decripted strin: %s\n", string);
+
+// 	return 0;
+// }
