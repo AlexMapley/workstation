d6ea1628fee6e77df223cba179bdf2dc7bd2d8ec
AlexMapley <alexmapley@gmail.com>
2019-09-24 12:36:49 -0400

    write vimrc in init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,5 @@
-perl setup/write_bashrc.pl && bash setup/installs/essentials.sh; source /root/.bashrc; perl art/scripts/starship.pl 
+# setup bashrc and install essentials
+perl setup/write_bashrc.pl && bash setup/installs/essentials.sh; source /root/.bashrc; perl art/scripts/starship.pl
+
+# write .vimrc
+cp setup/vimrc ~/.vimrc
