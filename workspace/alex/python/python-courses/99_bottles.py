860109c8352ccd4536ba5c0aa7ed514bdf5194c1
AlexMapley <alexmapley@gmail.com>
2019-09-25 11:04:29 -0400

    python: 99 bottles start

---
+++ workspace/alex/python/python-courses/99_bottles.py
@@ -0,0 +1,14 @@
+import os
+
+bob = "bottles of beer"
+otw = "on the wall"
+take1 = "Take one down, pass it around"
+store = "Go to the store and buy some more"
+
+def child(pipeout):
+    bottles = 99
+    while True:
+        if bottles > 0:
+            values = (bottles, bob, otw, bottles, bob, take1, bottles -1, bob, otw)
+            verse = 
+
