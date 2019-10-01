5df60aa68223005937b8818fdd0a7c711d67065a
AlexMapley <alexmapley@gmail.com>
2019-10-01 12:18:15 -0400

    finish go setup script

--- setup/installs/golang.sh
+++ setup/installs/golang.sh
@@ -3,4 +3,13 @@ wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
 tar -xvf go1.12.7.linux-amd64.tar.gz
 mv go /usr/local
 
+# Set path to golang binaries as well as GoPath
 export GOROOT=/usr/local/go
+export GOPATH=/src   
+
+# Allow go binary access system-wide 
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+# Verify go setup
+go version
+go env
