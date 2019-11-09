44b76f563a9a33b93d9b3db784c5510cc50b3ec8
AlexMapley <alexmapley@gmail.com>
2019-11-09 02:55:40 +0000

    move init to root directory

--- init.sh
+++
@@ -1,5 +0,0 @@
-# setup bashrc and install essentials
-perl setup/write_bashrc.pl && bash setup/installs/essentials.sh; source /root/.bashrc; perl art/scripts/starship.pl
-
-# write .vimrc
-cp setup/vimrc ~/.vimrc
