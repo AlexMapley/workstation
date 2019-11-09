618a7c7635374fe6fdc17be76adc3f5ecdf51333
AlexMapley <alexmapley@gmail.com>
2019-11-09 09:56:35 -0500

    remove go artifact on build install

--- setup/installs/golang.sh
+++ setup/installs/golang.sh
@@ -1,6 +1,7 @@
 # get go content and move to /usr/local
 wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
 tar -xvf go1.12.7.linux-amd64.tar.gz
+rm go1.12.7.linux-amd64.tar.gz
 mv ./go /bin/go
 
 # Set path to golang binaries as well as GoPath
