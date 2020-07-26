1777196409a6bf04e6fda5c9a9991d3f021ff1dc
AlexMapley <alexmapley@gmail.com>
2020-07-25 21:04:22 -0400

    fix escape pattern errors

--- setup/write_bashrc.pl
+++ setup/write_bashrc.pl
@@ -11,4 +11,4 @@ print $fd("alias py=\"python\"\n");
 
 print $fd("\n\n# Funcs\n");
 print $fd("parse_git_branch() {\n\tgit branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'\n}\n");
-print $fd("export PS1=\"\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ \"\n");
+print $fd("export PS1=\"\u@\\h \[\e[32m\]\\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ \"\n");
