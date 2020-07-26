2328940e4b48d7c22d53dfbd05a733236b73390a
AlexMapley <alexmapley@gmail.com>
2020-07-25 21:02:56 -0400

    add git ps1 to bashrc

--- setup/write_bashrc.pl
+++ setup/write_bashrc.pl
@@ -8,3 +8,7 @@ print $fd("alias c=\"clear\"\n");
 print $fd("alias src=\"cd /src\"\n");
 print $fd("alias python=\"python3\"\n");
 print $fd("alias py=\"python\"\n");
+
+print $fd("\n\n# Funcs\n");
+print $fd("parse_git_branch() {\n\tgit branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'\n}\n");
+print $fd("export PS1=\"\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ \"\n");
