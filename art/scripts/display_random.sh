95d95e8d9751eed07619d28f87965d3b5740c0b9
AlexMapley <alexmapley@gmail.com>
2020-07-27 22:08:39 -0400

    fix starship loader

--- art/scripts/display_random.sh
+++ art/scripts/display_random.sh
@@ -1,5 +1,5 @@
 cd ../ascii;
 printf "\n\n";
+perl /src/art/scripts/starship.pl
 ls -l | grep .txt | awk '{print $9}' | sort -R | tail -1 | xargs -L 1 cat
-bash /src/scripts/starship.pl
 printf "\n\n";
