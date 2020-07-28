dc185157405554171ff69c75ce45f0d66bf049d7
AlexMapley <alexmapley@gmail.com>
2020-07-27 21:51:34 -0400

    update writs_bashrc.pl

--- setup/installs/golang.sh
+++ setup/installs/golang.sh
@@ -1,7 +1,7 @@
 # get go content and move to /usr/local
 wget https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz
 shasum -a 256 go1.14.6.linux-amd64.tar.gz
-sudo tar -C /src/ -xvzf go1.7.3.linux-amd64.tar.gz
+sudo tar -C /src -xvzf go1.7.3.linux-amd64.tar.gz
 
 
 rm go1.12.7.linux-amd64.tar.gz
@@ -10,6 +10,6 @@ chmox +
 
 # Set path to golang binaries as well as GoPath
 export GOROOT=/src
-export GOPATH=/src   
+export GOPATH=/src
 export GOBIN=/src
 
