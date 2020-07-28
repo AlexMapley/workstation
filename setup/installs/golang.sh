26aac139b4864b6366edebef7a7f8e5978c1b149
AlexMapley <alexmapley@gmail.com>
2020-07-27 22:54:40 -0400

    improve go setup

--- setup/installs/golang.sh
+++ setup/installs/golang.sh
@@ -1,12 +1,15 @@
 # get go content and move to /usr/local
 wget https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz
+cd /
 shasum -a 256 go1.14.6.linux-amd64.tar.gz
-sudo tar -C /src -xvzf go1.7.3.linux-amd64.tar.gz
+# tar -C /src -xvzf go1.14.6.linux-amd64.tar.gz
 
 
-rm go1.12.7.linux-amd64.tar.gz
-mv ./go /bin/go
-chmox +
+tar -xf go1.14.6.linux-amd64.tar.gz
+rm go1.14.6.linux-amd64.tar.gz
+
+mv go /bin/go
+chmod +
 
 # Set path to golang binaries as well as GoPath
 export GOROOT=/src
